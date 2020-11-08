c7e405707d8fef267c49a82a4611b41f42a741e1
matkob <nologin90@gmail.com>
2020-11-08 18:21:52 +0100

    project restructured for R plugin

---
+++ classification/main.R
@@ -0,0 +1,5 @@
+# Title     : Testing Classifiers
+# Objective : Implement all the classification tools required for the project
+# Created by: MKobierski
+# Created on: 08.11.2020
+
---
+++ classification/stage2/preprocessing.R
@@ -0,0 +1,5 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: mati
+# Created on: 08.11.2020
+
