6b1f6068a8b53cd2c8f84e258bca8829052629d1
matkob <nologin90@gmail.com>
2020-11-13 11:41:38 +0100

    reg tree ready for tuning

--- classification/regression_tree.r
+++ classification/regression_tree.r
@@ -4,27 +4,31 @@ source("utils/loader_tfidf.r")
 source("utils/loader_glove.r")
 source("utils/measure_quality.r")
 
-sets_list_glove <- load_glove()
-train_X <- sets_list_glove[[1]][1:100,]
-train_y <- sets_list_glove[[2]][1:100]
-test_X <- sets_list_glove[[3]]
-test_y <- sets_list_glove[[4]]
+sets_list <- load_glove()
+train_X <- as.matrix(sets_list[[1]])
+train_y <- as.vector(sets_list[[2]])
+test_X <- as.matrix(sets_list[[3]])
+test_y <- as.vector(sets_list[[4]])
 
 train <- data.frame(cbind(train_X, train_y))
+test <- data.frame(cbind(test_X, test_y))
 
 tree_classifier <- rpart(
   train_y ~ .,
   data = train,
   method = 'class',
-  parms = list(split = 'information'),
-  maxdepth = 10
+  parms = list(split = 'information'), # to be chosen gini vs information
+  maxdepth = 10 # to be found
 )
 
-#printcp(tree_classifier) # display the results
-#pruned <- prune(tree_classifier, cp=tree_classifier$cptable[which.min(tree_classifier$cptable[,"xerror"]),"CP"])
-#printcp(pruned)
+pruned <- prune(
+  tree_classifier,
+  cp=10 # to be found
+)
 
-preds <- apply(predict(tree_classifier, data.frame(test_X)), 1, function (x) {
+
+preds <- predict(pruned, subset(test, select=-test_y))
+preds <- apply(preds, 1, function (x) {
   if (x[[1]] > x[[2]]) 0 else 1
 })
 measure_quality(preds, test_y)
