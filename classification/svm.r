bc0849c591d68bc4754e7ebbdd3b3676106d6520
matkob <nologin90@gmail.com>
2020-11-28 19:17:35 +0100

    svm with smaller dataset

--- classification/svm.r
+++ classification/svm.r
@@ -5,22 +5,28 @@ source("utils/loader_glove.r")
 source("utils/measure_quality.r")
 
 sets_list <- load_glove(oversampling = FALSE)
-#sets_list <- if (is.null(sets_list)) load_glove(oversampling = FALSE) else sets_list
 train_X <- sets_list[[1]]
 train_X <- train_X[1:as.integer(0.8*nrow(train_X)), ]
 train_y <- sets_list[[2]]
 train_y <- train_y[1:as.integer(0.8*length(train_y))]
+
+positive <- train_y == 1
+negative <- train_y == 0
+train_X <- rbind(train_X[positive, ][1:5000, ], train_X[negative, ][1:5000, ])
+train_y <- c(train_y[positive][1:5000], train_y[negative][1:5000])
 
 valid_X <- sets_list[[1]]
 valid_X <- valid_X[as.integer(0.8*nrow(valid_X)+1):nrow(valid_X), ]
 valid_y <- sets_list[[2]]
 valid_y <- valid_y[as.integer(0.8*length(valid_y)+1):length(valid_y)]
 
+positive <- valid_y == 1
+negative <- valid_y == 0
+valid_X <- rbind(valid_X[positive, ][1:5000, ], valid_X[negative, ][1:5000, ])
+valid_y <- c(valid_y[positive][1:5000], valid_y[negative][1:5000])
+
 test_X <- sets_list[[3]]
 test_y <- sets_list[[4]]
-
-train_n_positive <- sum(train_y == 1)
-train_n_negative <- sum(train_y == 0)
 
 svm_classifier <- svm(
   train_X,
@@ -28,8 +34,8 @@ svm_classifier <- svm(
   type = 'C-classification',
   kernel = 'radial',
   probability = TRUE,
-  cost=1,
-  gamma = 1/ncol(train_X)
+  cost=50,
+  gamma = 0.01/ncol(train_X)
 )
 
 print("############################## TRAIN ##############################")
