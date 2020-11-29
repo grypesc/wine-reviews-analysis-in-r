73b7e871964889009ff86977203ee6985d3c9fa4
grypesc <g.rypesc@wp.pl>
2020-11-29 12:48:30 +0100

    added pca to forest

--- classification/random_forest.r
+++ classification/random_forest.r
@@ -4,7 +4,7 @@ source("utils/loader_tfidf.r")
 source("utils/loader_glove.r")
 source("utils/measure_quality.r")
 
-sets_list <- load_glove(oversampling = FALSE)
+sets_list <- load_glove()
 train_X <- sets_list[[1]]
 train_X <- train_X[1:as.integer(0.8*nrow(train_X)), ]
 train_y <- sets_list[[2]]
@@ -15,9 +15,23 @@ valid_X <- valid_X[as.integer(0.8*nrow(valid_X)+1):nrow(valid_X), ]
 valid_y <- sets_list[[2]]
 valid_y <- valid_y[as.integer(0.8*length(valid_y)+1):length(valid_y)]
 
-c1 <- sum(train_y)
-c0 <- length(train_y) - c1
-class_ratio <- c0/c1
+full_train_X <- sets_list[[1]]
+full_train_y <- sets_list[[2]]
+
+test_X <- sets_list[[3]]
+test_y <- sets_list[[4]]
+
+### PCA ###
+#pca1 <- prcomp(train_X, scale. = TRUE)
+#train_X <- predict(pca1, train_X)
+#train_X <- train_X[, 1:200]
+#
+#valid_X <- predict(pca1, valid_X)
+#valid_X <- valid_X[, 1:200]
+#
+#test_X <- predict(pca1, test_X)
+#test_X <- test_X[, 1:200]
+###
 
 t1 <- Sys.time()
 forest_classifier <- randomForest(x = as.matrix(train_X), y = as.factor(train_y),
@@ -34,7 +48,8 @@ print("############################## VALID ##############################")
 valid_preds <- predict(forest_classifier, as.matrix(valid_X), type = 'prob')[, 2]
 measure_quality(as.numeric(as.character(valid_preds)), valid_y)
 
+source("utils/measure_quality.r")
+print("############################## TEST ##############################")
+test_preds <- predict(forest_classifier, as.matrix(test_X), type = 'prob')[, 2]
+measure_quality(as.numeric(as.character(test_preds)), test_y)
 
-#print("############################## TEST ##############################")
-#test_preds <- predict(forest_classifier, as.matrix(test_X), type = 'response')
-#measure_quality(as.numeric(as.character(test_preds)), test_y)
